9fd289664e03daf171ad517706fb53534a0f832a
Collum, Justin <Justin.Collum@cdk.com>
2015-10-18 15:06:07 -0700

    added in Mongo/Mongoose and the shooting schema; added in removing of existing entries during update added in push to Mongo during refresh data

--- services/googledocs.js
+++ services/googledocs.js
@@ -7,16 +7,19 @@ var config = require('../config');
 var fs = require('fs');
 var _ = require('lodash');
 var moment = require('moment');
-
 var Converter = require('csvtojson').Converter;
+var mongoUrl;
+var mongoose = require('mongoose');
 
 function GoogleDocs(app) {
   this.app = app || {};
 
   if (app.get('environment') === 'heroku') {
     this.url = process.env.GOOGLE_DOC_URL;
+    this.mongoUrl = process.env.MONGO_URL;
   } else {
     this.url = config['google-docs'].url;
+    mongoUrl = config['mongo'].url;
   }
 
   _.bindAll(this, 'refreshLocalData', 'getSheet', '_csvToJSON', '_writeJSONToFile');
@@ -27,11 +30,11 @@ module.exports = GoogleDocs;
 GoogleDocs.prototype.getSheet = function getSheet() {
 
   return nodefn.lift(request)({
-    uri: this.url
+    uri:this.url
   })
-  .then(function(result) {
-    return when.resolve(result[0].body);
-  });
+    .then(function (result) {
+      return when.resolve(result[0].body);
+    });
 
 };
 
@@ -39,9 +42,9 @@ GoogleDocs.prototype._csvToJSON = function _csvToJSON(csvStr) {
 
   var converter = new Converter({});
 
-  return when.promise(function(resolve, reject) {
+  return when.promise(function (resolve, reject) {
 
-    converter.fromString(csvStr, function(err, result){
+    converter.fromString(csvStr, function (err, result) {
 
       if (err) {
         reject(err);
@@ -60,29 +63,29 @@ GoogleDocs.prototype.refreshLocalData = function refreshLocalData() {
   var now = moment();
 
   return this.getSheet()
-    .then(function(sheetStr) {
+    .then(function (sheetStr) {
       return _this._csvToJSON(sheetStr);
     })
-    .then(function(result) {
+    .then(function (result) {
 
       var thisYear = String(now.year());
 
-      var numShootingsThisYear = _.filter(result, function(shooting) {
+      var numShootingsThisYear = _.filter(result, function (shooting) {
         return shooting.date.indexOf(thisYear) > -1;
       }).length;
 
-      result = _.sortBy(result, function(item) {
+      result = _.sortBy(result, function (item) {
         return -1 * moment(new Date(item.date)).unix();
       })
 
       var daysSinceLastShooting = moment(new Date(result[0].date)).fromNow().split(' ')[0];
 
-      result = _.map(result, function(item) {
+      result = _.map(result, function (item) {
 
         item.location = item.city + ', ' + item.state;
 
         item.sources = item.sources_csv.split(',');
-        item.sources = _.map(item.sources, function(src) {
+        item.sources = _.map(item.sources, function (src) {
           return src.trim();
         })
 
@@ -90,12 +93,74 @@ GoogleDocs.prototype.refreshLocalData = function refreshLocalData() {
       });
 
       var data = {
-        numShootingsThisYear: numShootingsThisYear,
-        daysSinceLastShooting: parseInt(daysSinceLastShooting),
-        shootings: result
+        numShootingsThisYear:numShootingsThisYear,
+        daysSinceLastShooting:parseInt(daysSinceLastShooting),
+        shootings:result
       }
 
-      return _this._writeJSONToFile(data);
+      mongoose.connect(mongoUrl);
+      var db = mongoose.connection;
+      var Shooting = require('.././data/schema/shooting')
+      db.on('error', console.error.bind(console, 'connection error:'));
+      db.once('open', function (callback) {
+        console.log('connected to mongo; proceeding')
+        var d, i, len, ref;
+
+        ref = data.shootings;
+        var total = ref.length;
+        var checked = 0;
+        var n = 0;
+
+        for (i = 0, len = ref.length; i < len; i++) {
+          d = ref[i];
+          var entry = new Shooting();
+          entry.date = new Date(d.date);
+          entry.perpetrators = [{name:d.name}];
+          entry.killed = d.killed;
+          entry.city = d.city;
+          entry.wounded = d.wounded;
+          entry.city = d.location.split(',')[0];
+          entry.state = (d.location.split(',')[1]).trim();
+          entry.sources = d.sources;
+
+          // find any entries on this data with the same city and state and at least one matching source
+          // if found, delete and re-create
+          // else just save each one
+          var upsert = function (entry) {
+
+            Shooting.find({
+              city:entry.city,
+              state:entry.state,
+              date:entry.date,
+              sources:{$in:entry.sources}
+            }).exec(function (err, shooting) {
+              // if it exists, delete it
+              if (err) throw 'error in Shooting find: ' + err;
+              else if (shooting.length > 1) {
+                console.dir(entry) // probably needs an IIFE
+                throw 'found multiple entries! (this should be very rare at best)'
+              }
+              else if (shooting.length > 0) {
+                Shooting.remove({_id:shooting[0]._id}, function (err, result) {
+                  entry.save()
+                });
+              }
+              else {
+                ++n;
+                entry.save();
+              }
+              ++checked;
+              if (checked == total) {
+                console.log("done writing data to Mongo (" + n + " new records)");
+                _this._writeJSONToFile(data);
+              }
+            })
+          };
+
+          upsert(entry);
+
+        }
+      });
     });
 };
 
