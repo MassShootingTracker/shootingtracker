374035c4a058e0781d140106521bce47272bcb79
Collum, Justin <Justin.Collum@cdk.com>
2015-10-27 01:43:13 -0700

    fixed an issue that was causing redis totals to be ignored and pulled from mongo every time added dataLayer interaction in to index controller added changes to recentshootings partial (needs work) changed index.hbs to use new data structure removed use of shootings.json

--- controllers/index.js
+++ controllers/index.js
@@ -2,25 +2,38 @@
 
 var _ = require('lodash');
 var Api = require('./api');
+var path = require('path');
+var config = require(path.join(process.cwd(),'./config' ))
+var dataLayer = null;
+var app = null
 
-function Index(app) {
-  this.app = app;
-
+function Index(appArg) {
+  this.app = appArg;
+  dataLayer = new (require(path.join(process.cwd(),'./services/data.js' ))).Data(config);
+  app = this.app;
   _.bindAll(this, 'home');
 }
 
 module.exports = Index;
 
 Index.prototype.register = function() {
+  var datapage = this.datapage;
   this.app.get('/', this.home);
   this.app.get('/about', this.aboutus);
   this.app.get('/data', this.datapage);
+  this.app.get('/:year(\\d{4})/', function (req, res){
+    datapage(req, res);
+  });
   (new Api(this.app)).register();
 }
 
 Index.prototype.home = function home(req, res, next) {
-  console.log("render index... for the moment, just year 2015")
-  res.render('index');
+  dataLayer.getTotals().then(function (data) {
+    app.locals.data = data;
+    data.currentYear = data[new Date().getFullYear()];
+    //console.dir(data);
+    res.render('index');
+  });
 }
 
 Index.prototype.aboutus = function aboutus(req, res, next) {
@@ -28,5 +41,10 @@ Index.prototype.aboutus = function aboutus(req, res, next) {
 }
 
 Index.prototype.datapage = function datapage(req, res, next) {
-  res.render('data');
+  console.log('datapage, param: ' + req.params.year);
+  dataLayer.getByYear(+req.params.year).then(function (data) {
+    app.locals.data = data;
+    //console.dir(data[0])
+    res.render('data');
+  });
 }
