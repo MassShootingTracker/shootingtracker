5a08cdcc0f4033cc302a10dc43303e9b6ebcada7
Collum, Justin <Justin.Collum@cdk.com>
2016-06-13 15:04:07 -0700

    added some code (that may be used later) to deal with ports for multiple processes

--- config/index.js
+++ config/index.js
@@ -11,12 +11,22 @@ try {
 def = require('./default.json');
 conf = ld.merge(conf, def);
 
+if (process.env.pm_id) {
+  console.log({"process.env.pm_id":process.env.pm_id});
+}
+
 if (process.env.OPENSHIFT_NODEJS_IP) {
   conf.app.hostname = process.env.OPENSHIFT_NODEJS_IP;
 }
 if (process.env.OPENSHIFT_NODEJS_PORT) {
   conf.app.port = process.env.OPENSHIFT_NODEJS_PORT;
 }
+
+// set port based on pm2 process ID?
+if (false && process.env.pm_id){
+  conf.app.port = +conf.app.port + +process.env.pm_id;
+}
+
 if (process.env.OPENSHIFT_APIKEY) {
   conf.app.apiKey = process.env.OPENSHIFT_APIKEY;
 }
@@ -45,19 +55,19 @@ if (process.env.LOG_LEVEL) {
   conf.logLevel = "warn"
 }
 
+console.log("Log level set to: "+conf.logLevel);
+
 conf.logger = (require('bunyan')).createLogger({
-  name: "mst",
-  streams: [
+  name:"mst",
+  streams:[
     {
-      level: conf.logLevel,
-      stream: process.stdout
+      level:conf.logLevel,
+      stream:process.stdout
     }, {
-      level: "error",
-      path: (conf.app.loggingPath + 'mst.log' || "/var/tmp/mst-error.log")
+      level:"error",
+      path:(conf.app.loggingPath + 'mst.log' || "/var/tmp/mst-error.log")
     }
   ]
 });
-
-
 
 module.exports = conf;
